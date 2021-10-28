a5acbb0dac3ccd3db591bc7684d221d4ede33123
hyoguen <ohg89416@gmail.com>
2021-10-28 17:33:19 +0900

    2021-10-28

---
+++ app/src/main/java/com/example/bunsinsaba/StartActivity.java
@@ -0,0 +1,17 @@
+package com.example.bunsinsaba;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StartActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        Intent intent = getIntent();
+
+    }
+
+}
